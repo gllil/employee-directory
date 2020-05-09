929ac4bc50ee86706b23a673578f8ad38d822c05
Gary Llil <garyjllil@gmail.com>
2020-05-09 15:43:48 -0400

    fixed search

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,52 +2,29 @@ import React from "react";
 import Wrapper from "./components/Wrapper";
 import NavBar from "./components/NavBar";
 import SearchForm from "./components/SearchForm";
-import Employees from "./components/Employees";
-import employee from "./employee.json"
-import "./components/Employees/style.css";
+// import Employees from "./components/Employees";
+import "./App.css";
+
 
 class App extends React.Component {
 
-    state = {
-        employee
-    }
+
+
 
     render(){
+    
+    
     return (
         <Wrapper>
             <NavBar/>
-            <SearchForm />
-            <div className="container">
-            <div className="row">
-                <div className="col">
-                <table class="table table-striped">
-                <thead>
-                    <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">DOB</th>
-                    </tr>
-                </thead>
-            {this.state.employee.map(empInfo => (
-            <Employees 
-                image={empInfo.picture.thumbnail}
-                name={empInfo.name.first} 
-                phone={empInfo.phone}
-                email={empInfo.email}
-                dob={Date(empInfo.dob.date)}
+            <SearchForm/>
 
-            />
-            ))}
-                     </table>
-                </div>
-            </div>
-        </div>
         </Wrapper>
         )
     }
 }
+
+
 
 
 export default App;
--- employee-directory/src/components/SearchForm/index.js
+++ employee-directory/src/components/SearchForm/index.js
@@ -1,31 +1,69 @@
 import React from "react";
+import Employees from "../Employees"
+import Moment from "moment";
 import "./style.css"
-
+import employee from "../../employee.json"
+import Table from "react-bootstrap/table"
 class SearchForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ""};
-
-        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            employee: employee,
+            search: "",
+        }      
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    updateSearch(event) {
+        this.setState({search: event.target.value})
     }
 
-    render() {
+    handleNameSort(emp) {
+        
+        this.setState({search: emp.sort()})
+    }
+
+    render(){
+        let filteredEmployees = this.state.employee.filter(emp => emp.name.first.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 || emp.name.last.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)
+        Moment.locale("en")
         return (
             <div className="container">
                 <div className="row justify-content-md-center">
                     <div className="col-sm3">
                     <form className="text-center">
-                        <input type="text" placeholder="Search" className="form-control"value={this.state.value} onChange={this.handleChange}/>
+                        <input type="text" placeholder="Search" className="form-control"value={this.state.search} onChange={this.updateSearch.bind(this)}/>
                     </form>
                     </div>
                 </div>
+            <div className="row">
+                <div className="col">
+                <Table className="tableStyle text-center" striped>
+                        <thead>
+                            <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col"><span onClick={filteredEmployees.handleNameSort}>Name</span></th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">DOB</th>
+                            </tr>
+                        </thead>
+                {filteredEmployees.map(empInfo => (                          
+                            <Employees 
+                                key={empInfo.id.value}
+                                image={empInfo.picture.thumbnail}
+                                name={`${empInfo.name.first} ${empInfo.name.last}`} 
+                                phone={empInfo.phone}
+                                email={empInfo.email}
+                                dob={Moment(empInfo.dob.date).format("M/D/YYYY")}
+                            />
+                            ))}
+                     </Table>
+                </div>
             </div>
+
+        </div>
+
         )
-    }
+        }
 
 }
 
