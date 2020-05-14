54e70ce43f9ff9f9056a164edf640efdaa6c5521
Gary Llil <garyjllil@gmail.com>
2020-05-14 14:24:34 -0400

    fixed heading fluidity

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function NavBar(props){
     return (
-        <div className="navbar text-center">
+        <div className="navbar text-center container-fluid">
             <div className="navbar-brand h1 mb-0 text-center title">Employee Directory</div>
             <p className="text-center subtitle"><small>Click on carrots to filter by heading or use the search box to narrow your results</small></p>
         </div>
